ac7855dd9e3eeaeb0ae3506e588295a778b8c046
Infernaton <lubin.pauly@gmail.com>
2021-02-08 18:05:07 +0100

    Comment

--- js/shoppping.js
+++ js/shoppping.js
@@ -6,35 +6,37 @@ console.log("addcart")
     console.log("hello");
 }
 }*/
-
-if (localStorage.getItem("list") == null){
-    localStorage.setItem("list", 0);
+//"nbObject", in the storage, represents the number of article in our cart. So, if there no nbObject key, we create one at first.
+if (localStorage.getItem("nbObject") == null){
+    localStorage.setItem("nbObject", 0);
 }
-let nbObject = localStorage.getItem("list");
+let nbObject = localStorage.getItem("nbObject");
 
+//Display of the article in our cart
 let text = '';
 let content = document.getElementById("contentCart");
 for (let i=1; i<= nbObject; i++){
     idCourse = localStorage.getItem(`${i}`)
     text = text +' '+ COURSES[idCourse].title + '\n';
 }
-
 content.innerHTML = text;
 
 function addcart (idCourse){
     let deny = false;
+    //Test to not buy an other same product we have in the cart
     for (let i=1; i<=nbObject; i++){
         if(localStorage.getItem(`${i}`) == idCourse-1){
             deny = true;
-            console.log("Already in your cart")
+            console.log("Already in your cart");
             break;
         }
     }
+    //Then, add it
     if (deny == false){
         nbObject++;
         addToCart(nbObject,idCourse-1);
-        localStorage.setItem("list", nbObject);
-        location.reload()
+        localStorage.setItem("nbObject", nbObject); //Upgrade the value in the storage
+        location.reload() // Used to refresh the page, else the cart will not apply the current product that we add
     }
 }
 function addToCart (nbObject,course){  
