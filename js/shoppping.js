f0392da31648e4930c71cda84849ab6ab1c825c7
Infernaton <lubin.pauly@gmail.com>
2021-02-09 15:25:55 +0100

    Display the total price in the cart

--- js/shoppping.js
+++ js/shoppping.js
@@ -8,14 +8,16 @@ let nbObject = localStorage.getItem("nbObject");
 
 let text = '<tr>';
 let content = document.getElementById("contentCart");
+let price = 0;
 
 for (let i=1; i<= nbObject; i++){
     let courseCart = JSON.parse(localStorage.getItem(`${i}`))
     //Display like a table in the cart:         The Title                      The price                   The quantity
     let courseCartTable = `<th></th> <th>  ${courseCart.title}  </th>  <th> ${courseCart.price}   </th>  <th>  ${1}  </th>  <th><a onclick = "removeOne(${i})" id="btsup" class="bt-supprimer">Supprimer</a></th>`
     text = text +' '+ courseCartTable + '</tr>';
+    price += courseCart.price*1;
 }
-
+text = text + `<th></th> <th>Total </th> <th> ${price} €</th>`;
 content.innerHTML = text ;
 
 
@@ -86,7 +88,7 @@ function remove(key)
 }
 
 function removeall(){
-    // empty basket function
+    // empty cart function
     localStorage.clear() // empties all stored keys
     location.reload(); // Used to refresh the page, else the cart will not apply the current product that we add
 }
