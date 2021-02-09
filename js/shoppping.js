09ae69ecdde58184ac542e6f39abdfb0d2295ac4
Infernaton <lubin.pauly@gmail.com>
2021-02-09 11:43:01 +0100

    Stock a json object in the storage rather than a ID

--- js/shoppping.js
+++ js/shoppping.js
@@ -5,19 +5,21 @@ if (localStorage.getItem("nbObject") == null){
 let nbObject = localStorage.getItem("nbObject");
 
 //Display of the article in our cart
+
 let text = '';
 let content = document.getElementById("contentCart");
 for (let i=1; i<= nbObject; i++){
-    idCourse = localStorage.getItem(`${i}`)
-    text = text +' '+ COURSES[idCourse].title + '<br>';
+    let courseCart = JSON.parse(localStorage.getItem(`${i}`))
+    text = text +' '+ courseCart.title + '<br>';
 }
 content.innerHTML = text;
+
 
 function addcart (idCourse){
     let deny = false;
     //Test to not buy an other same product we have in the cart
     for (let i=1; i<=nbObject; i++){
-        if(localStorage.getItem(`${i}`) == idCourse-1){
+        if(JSON.parse(localStorage.getItem(`${i}`)).id == COURSES[idCourse-1].id){
             deny = true;
             console.log("Already in your cart");
             break;
@@ -27,7 +29,7 @@ function addcart (idCourse){
     if (deny == false){
         if ( confirm( "Are you sure to add " + COURSES[idCourse-1].title + " ?") ) {
             nbObject++;
-            addToCart(nbObject,idCourse-1); //Save the ID of the course that we buy
+            addToCart(nbObject,JSON.stringify(COURSES[idCourse-1])); //Save the ID of the course that we buy
             localStorage.setItem("nbObject", nbObject); //Upgrade the value in the storage
             location.reload() // Used to refresh the page, else the cart will not apply the current product that we add
         }else{
