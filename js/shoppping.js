7390e6248343bdf723366f959b46460180c017f6
Infernaton <lubin.pauly@gmail.com>
2021-02-09 13:57:29 +0100

    Update shoppping.js

--- js/shoppping.js
+++ js/shoppping.js
@@ -11,11 +11,11 @@ let content = document.getElementById("contentCart");
 
 for (let i=1; i<= nbObject; i++){
     let courseCart = JSON.parse(localStorage.getItem(`${i}`))
-    //Display like a table in the cart
-    let courseCartTable = `<th></th> <th>  ${courseCart.title}  </th>  <th> ${courseCart.price}   </th>  <th>  ${1}  </th>  <th><a onclick = "remove(${i})" id="btsup" class="bt-supprimer">Supprimer</a></th>`
+    //Display like a table in the cart:         The Title                      The price                   The quantity
+    let courseCartTable = `<th></th> <th>  ${courseCart.title}  </th>  <th> ${courseCart.price}   </th>  <th>  ${1}  </th>  <th><a onclick = "removeOne(${i})" id="btsup" class="bt-supprimer">Supprimer</a></th>`
     text = text +' '+ courseCartTable + '</tr>';
 }
-console.log(content);
+
 content.innerHTML = text ;
 
 
@@ -23,6 +23,7 @@ function addcart (idCourse){
     let deny = false;
     //Test to not buy an other same product we have in the cart
     for (let i=1; i<=nbObject; i++){
+        console.log(JSON.parse(localStorage.getItem(`${i}`)));
         if(JSON.parse(localStorage.getItem(`${i}`)).id == COURSES[idCourse-1].id){
             deny = true;
             console.log("Already in your cart");
@@ -41,17 +42,18 @@ function addcart (idCourse){
         }
     }
 }
-
 function addToCart (nbObject,course){  
     localStorage.setItem(nbObject,course);
     console.log("addcart");
 }
 
-function remove(key){ // delete button function
-    localStorage.removeItem(key) // deletes the specified storage object item
-    console.log("remove key")
+function removeOne(key){
+    remove(key);
     nbObject--;
     localStorage.setItem("nbObject", nbObject);//Upgrade the value in the storage
     location.reload(); // Used to refresh the page, else the cart will not apply the current product that we add
-    
+}
+function remove(key){ // delete button function
+    localStorage.removeItem(key) // deletes the specified storage object item
+    console.log("remove key")
 }
