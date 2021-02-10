25ad11fa699df0579a0aeffe1dae9f2b0c0d5633
Infernaton <lubin.pauly@gmail.com>
2021-02-10 16:50:39 +0100

    Uses of JQuery with the search bar

--- js/search.js
+++ js/search.js
@@ -1,29 +1,26 @@
 function search() {
     // Declare variables
     let a, txtValue;
-    let count =0;
+    let count = 0;
     let input = document.getElementById('searchBar');
     let filter = input.value.toUpperCase();
-
-    let listCourses = document.getElementById("courses_list");
-    let courseItem = listCourses.getElementsByClassName('course__item');
-  
+    
+    $(document).ready(function(){$('.course__item').fadeOut(300);});
     // Loop through all list items, and hide those who don't match the search query
-    for (let i = 0; i < courseItem.length; i++) {
-
-        a = courseItem[i].getElementsByTagName("h4")[0];
+    for (let i = 0; i < $('.course__item').length; i++) {
+        a = $('.course__item').eq(i)[0].getElementsByTagName("h4")[0];
         txtValue = a.textContent || a.innerText; //To get the complete title of the course
 
-        if (txtValue.toUpperCase().indexOf(filter) <= -1) { //The test if the research and the title match
-            courseItem[i].classList.add("hidden");
-            count ++;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) { //The test if the research and the title match
+            $(document).ready(function(){$('.course__item').eq(i).fadeIn();}); 
         } else {
-            courseItem[i].classList.remove("hidden");
+            $(document).ready(function(){$('.course__item').eq(i).fadeOut();});
+            count ++;
         }
     }
-    if (count == courseItem.length){
-        document.getElementById("no_course").classList.remove('hidden');
+    if (count == $('.course__item').length){
+        $(document).ready(function(){$("#no_course").fadeIn().removeClass("hidden");});
     }else {
-        document.getElementById("no_course").classList.add('hidden');
+        $(document).ready(function(){$("#no_course").fadeOut().addClass("hidden");});
     }
 }
