f64073066771517b2ef05320c54693d228bddb92
Sory <sorybagayoko5@gmail.com>
2021-02-11 15:13:02 +0100

    commantary timer

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,6 @@
 const FULL_DASH_ARRAY = 283;
-const WARNING_THRESHOLD = 10;
-const ALERT_THRESHOLD = 5;
+const WARNING_THRESHOLD = 180;
+const ALERT_THRESHOLD = 90;
 
 const COLOR_CODES = {
   info: {
@@ -15,10 +15,14 @@ const COLOR_CODES = {
     threshold: ALERT_THRESHOLD
   }
 };
-
+//Start with an initial value of 20 seconds
 const TIME_LIMIT = 300;
+
+// Initially, no time has passed, but this will count up
+// and subtract from the TIME_LIMIT
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
+
 let timerInterval = null;
 let remainingPathColor = COLOR_CODES.info.color;
 
@@ -54,8 +58,12 @@ function onTimesUp() {
 
 function startTimer() {
   timerInterval = setInterval(() => {
+
+    // The amount of time passed increments by one
     timePassed = timePassed += 1;
     timeLeft = TIME_LIMIT - timePassed;
+
+    // The time left label is updated
     document.getElementById("base-timer-label").innerHTML = formatTime(
       timeLeft
     );
@@ -69,18 +77,23 @@ function startTimer() {
 }
 
 function formatTime(time) {
+    // The largest round integer less than or equal to the result of time divided being by 60.
   const minutes = Math.floor(time / 60);
+  // Seconds are the remainder of the time divided by 60 (modulus operator)
   let seconds = time % 60;
 
+    // If the value of seconds is less than 10, then display seconds with a leading zero
   if (seconds < 10) {
     seconds = `0${seconds}`;
   }
-
+// The output in MM:SS format
   return `${minutes}:${seconds}`;
 }
 
 function setRemainingPathColor(timeLeft) {
   const { alert, warning, info } = COLOR_CODES;
+
+  // If the remaining time is less than or equal to 5, remove the "warning" class and apply the "alert" class.
   if (timeLeft <= alert.threshold) {
     document
       .getElementById("base-timer-path-remaining")
@@ -88,6 +101,7 @@ function setRemainingPathColor(timeLeft) {
     document
       .getElementById("base-timer-path-remaining")
       .classList.add(alert.color);
+      // If the remaining time is less than or equal to 10, remove the base color and apply the "warning" class.
   } else if (timeLeft <= warning.threshold) {
     document
       .getElementById("base-timer-path-remaining")
@@ -97,12 +111,12 @@ function setRemainingPathColor(timeLeft) {
       .classList.add(warning.color);
   }
 }
-
+// Divides time left by the defined time limit.
 function calculateTimeFraction() {
   const rawTimeFraction = timeLeft / TIME_LIMIT;
   return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
 }
-
+// Update the dasharray value as time passes, starting with 283
 function setCircleDasharray() {
   const circleDasharray = `${(
     calculateTimeFraction() * FULL_DASH_ARRAY
