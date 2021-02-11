7846e74494f0f07b31b36e833a32373a7447b688
Infernaton <lubin.pauly@gmail.com>
2021-02-11 18:59:00 +0100

    Fix the submit button to validate the command

--- js/order.js
+++ js/order.js
@@ -3,20 +3,23 @@ function suivant(enCours, suivant, limite) {
         document.code[suivant].focus();
     }
 }
+$(document).ready(function(){
+    $("#div2").addClass("hidden");});
 
-    function redirection(){
-    $(document).ready(function(){
-        $("#div2").atclass("hidden");
-
-    $(document).ready(function(){
+$(document).ready(function(){
+    $("#empty-cart").click(function(){
         $("#div1").fadeOut();
-        $("#div2").fadeIn();});
-        
-            
+        $("#div2").fadeIn(2000).removeClass("hidden");
+    })
+    /*$("#data").click(function(){
+        redirection();
+    })*/
+});
+
+function redirection(){
     if (price >= 100){
         console.log("start the timer")
         localStorage.setItem("istimeractive", true);
     }
     removeall('stockTaken');
 }
-    )}
