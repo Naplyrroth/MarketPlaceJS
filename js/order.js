4f826f27d6d4823e62f4c4cf6bed2e16e4093787
nairb725 <58400498+nairb725@users.noreply.github.com>
2021-02-12 09:21:13 +0100

    Update order.js

--- js/order.js
+++ js/order.js
@@ -1,11 +1,15 @@
+//go to the next text area
 function suivant(enCours, suivant, limite) {
     if (enCours.value.length >= limite){
         document.code[suivant].focus();
     }
 }
+
+//hide div2 = credit cart information
 $(document).ready(function(){
     $("#div2").addClass("hidden");});
 
+//delte cart with and reveal credit cart information 
 $(document).ready(function(){
     $("#empty-cart").click(function(){
         $("#div1").fadeOut();
@@ -16,6 +20,7 @@ $(document).ready(function(){
     })*/
 });
 
+//promotion
 function redirection(){
     if (price >= 100){
         console.log("start the timer")
